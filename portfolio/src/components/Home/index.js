a04d13df9de40d45f51bc1f97f050957653fbf67
sneha <snehamittal88293@gmail.com>
2023-04-19 08:34:30 +0530

    Building Home page

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,3 +1,22 @@
-const Home = () => ('Hello')
+import {Link} from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.sccs';
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi ,  <br/> I'm
+                <img src={LogoTitle} alt="developer"/>
+                neha Mittal
+                <br/>
+                web developer
+                </h1>
+                <h2>Frontend Developer / Java</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
 
 export default Home
