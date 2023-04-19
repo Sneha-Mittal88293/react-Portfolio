6ba23de1cf4c2e16840f3b50d84b3be55e7d44a6
sneha <snehamittal88293@gmail.com>
2023-04-19 22:09:19 +0530

    home page logo

--- portfolio/src/components/Home/index.js
+++ portfolio/src/components/Home/index.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
-import './index.scss'
 import AnimatedLetters from '../AnimatedLetters'
+import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -36,6 +37,7 @@ const Home = () => {
                 <h2>Frontend Developer / Java</h2>
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
+            <Logo/>
         </div>
     );
 }
